9e0c4f85536c6088a3ae81dd15242281d09874cb
hangillee <skfcb10@naver.com>
2023-01-14 07:12:36 +0900

    chore: edit styles and social media hyperlinks

--- packages/core/constants/Colors/index.ts
+++ packages/core/constants/Colors/index.ts
@@ -6,7 +6,7 @@ export const lightCode: ColorType = null;
 export const lightCodeLight: ColorType = null;
 export const lightBackground: ColorType = null;
 
-export const darkPrimary: ColorType = null;
+export const darkPrimary: ColorType = '#6db33f';
 export const darkSelection: ColorType = '#6db33f';
 export const darkCode: ColorType = '#6db33f';
 export const darkCodeLight: ColorType = '#80ea6e';
