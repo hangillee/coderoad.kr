8a790185ad8f40296260d10a14a593f4d2ad0bf2
hangillee <skfcb10@naver.com>
2023-01-14 07:23:56 +0900

    chore: edit inline code style

--- packages/core/constants/Colors/index.ts
+++ packages/core/constants/Colors/index.ts
@@ -2,12 +2,12 @@ type ColorType = string | null;
 
 export const lightPrimary: ColorType = '#6db33f';
 export const lightSelection: ColorType = '#6db33f';
-export const lightCode: ColorType = '#6db33f';
-export const lightCodeLight: ColorType = '#80ea6e';
+export const lightCode: ColorType = '#ffffff';
+export const lightCodeLight: ColorType = '#6db33f';
 export const lightBackground: ColorType = null;
 
 export const darkPrimary: ColorType = '#6db33f';
 export const darkSelection: ColorType = '#6db33f';
-export const darkCode: ColorType = '#6db33f';
-export const darkCodeLight: ColorType = '#80ea6e';
+export const darkCode: ColorType = '#ffffff';
+export const darkCodeLight: ColorType = '#6db33f';
 export const darkBackground: ColorType = null;
