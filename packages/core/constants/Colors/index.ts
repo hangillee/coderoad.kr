7a680a6993357b73cc0caab34488ff88da499020
hangillee <skfcb10@naver.com>
2023-01-23 03:07:25 +0900

    chore: edit post's context and blog configuration

--- packages/core/constants/Colors/index.ts
+++ packages/core/constants/Colors/index.ts
@@ -2,12 +2,12 @@ type ColorType = string | null;
 
 export const lightPrimary: ColorType = '#6db33f';
 export const lightSelection: ColorType = '#6db33f';
-export const lightCode: ColorType = '#ffffff';
-export const lightCodeLight: ColorType = '#6db33f';
+export const lightCode: ColorType = '#d4f088';
+export const lightCodeLight: ColorType = '#191e1e';
 export const lightBackground: ColorType = null;
 
 export const darkPrimary: ColorType = '#6db33f';
 export const darkSelection: ColorType = '#6db33f';
-export const darkCode: ColorType = '#ffffff';
-export const darkCodeLight: ColorType = '#6db33f';
+export const darkCode: ColorType = '#a1d21b';
+export const darkCodeLight: ColorType = '#262a2d';
 export const darkBackground: ColorType = null;
