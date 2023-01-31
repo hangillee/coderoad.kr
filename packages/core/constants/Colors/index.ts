1f83cd8a016ce7098f878c88dd0e167a7853242f
hangillee <skfcb10@naver.com>
2023-02-01 02:10:34 +0900

    chore: edit style

--- packages/core/constants/Colors/index.ts
+++ packages/core/constants/Colors/index.ts
@@ -2,8 +2,8 @@ type ColorType = string | null;
 
 export const lightPrimary: ColorType = '#6db33f';
 export const lightSelection: ColorType = '#6db33f';
-export const lightCode: ColorType = '#87837826';
-export const lightCodeLight: ColorType = '#eb5757';
+export const lightCode: ColorType = '#eb5757';
+export const lightCodeLight: ColorType = '#87837826';
 export const lightBackground: ColorType = null;
 
 export const darkPrimary: ColorType = '#6db33f';
